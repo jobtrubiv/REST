1e9f00ffc0a6668102bbcec9773495720afe2188
trubiv <trubiv>
2017-05-12 16:25:18 +0300

    new init

---
+++ get.php
@@ -0,0 +1,38 @@
+<?php
+if ($_POST["server"] == "zomart") {
+	$requestURL = "https://zomart.ru/.api/v1/";
+}else{
+	$requestURL = "http://dev04.zomart.ru/.api/v1/";
+}
+if ($_POST["metod"] == "category") {
+	$reqest = array("jsonrpc" =>"2.0", "method"=>"getRubric", "params"=> array("rubricId"=> "-5"), "id"=>1);
+	if( $curl = curl_init() ) {
+		curl_setopt($curl, CURLOPT_URL, $requestURL);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($reqest));
+		$out = curl_exec($curl);
+		curl_close($curl);
+		$result = json_decode($out);
+		$answer = array();
+		$answer["head"] = "<div class='row'><div class='col-md-3 col-md-offset-1'>jsonrpc:</div><div class='col-md-6'>$result->jsonrpc</div></div><br>";
+		$answer["head"] .= "<div class='row'><div class='col-md-3 col-md-offset-1'>id:</div><div class='col-md-6'>$result->id:</div></div><br>";
+		$str = "";
+	foreach ($result->result as $key => $value) {
+		if (!is_array($value)) {
+			$str .= "<div class='row'><div class='col-md-3 col-md-offset-2'>$key</div><div class='col-md-6'>$value</div></div><br>";
+		}else{
+			foreach ($value as $arr => $ob) {
+				$str .= "<div class='row'><div class='col-md-3 col-md-offset-3'>id</div><div class='col-md-6'>$ob->id</div></div><br>";
+				$str .= "<div class='row'><div class='col-md-3 col-md-offset-3'>name</div><div class='col-md-6'>$ob->name</div></div><br>";
+				if (isset($ob->hasChildren)) {
+					$str .= "<div class='row'><div class='col-md-3 col-md-offset-3'>hasChildren</div><div class='col-md-6'>$ob->hasChildren</div></div><br>";
+				}
+			}
+		}
+	}
+		$answer["body"] =$str;
+	die(json_encode(array("head" => $answer["head"], "body" => $answer["body"],)));
+	}
+}
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>тест REST Zоmart</title>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="panel panel-default">
+		  <div class="panel-heading">getRubric 
+				<button type="button" class="btn btn-primary" onclick="btnCat()">запрос корневых категорий </button>
+				<button type="button" class="btn btn-info" onclick="btnCatDev()">запрос корневых категорий dev</button>
+			</div>
+		  <div class="panel-body">
+
+				  <!-- <div class="col-md-9">
+				    Ответ на запрос {"jsonrpc":"2.0","method":"getRubric","params":{"rubricId":-5},"id":1}; <br>
+						<br>
+				    <div class="head"></div>
+				    <div class="body"></div>
+
+				  </div> -->
+
+					<!-- <div class="row">
+						<div class="col-md-3 col-md-offset-1">
+							Тело ответа:
+						</div>
+						<div class="col-md-6">
+							<div class="answer"></div>
+						</div>
+					</div> -->
+
+		  </div>
+			</div>
+		</div>
+	</div>
+	<script>
+		function btnCat(){
+			$(".panel-body").html('<div class="col-md-9">Ответ zomart.ru на запрос {"jsonrpc":"2.0","method":"getRubric","params":{"rubricId":-5},"id":1};<br><br><div class="head"></div><div class="body"></div></div>');
+			$.ajax({
+				url: "get.php",
+				method: "POST",
+				data: {
+					metod: "category",
+					server: "zomart"
+							},
+				success: function (data) {
+					console.log(data);
+					 var answer = JSON.parse(data);
+					 $(".head").html(answer.head);
+					 $(".body").html(answer.body);
+    			}
+			});
+		}
+	</script>
+	<script>
+		function btnCatDev(){
+			$(".panel-body").html('<div class="col-md-9">Ответ dev04.zomart.ru на запрос {"jsonrpc":"2.0","method":"getRubric","params":{"rubricId":-5},"id":1};<br><br><div class="head"></div><div class="body"></div></div>');
+			$.ajax({
+				url: "get.php",
+				method: "POST",
+				data: {
+					metod: "category",
+					server: "dev"
+							},
+				success: function (data) {
+					console.log(data);
+					 var answer = JSON.parse(data);
+					 $(".head").html(answer.head);
+					 $(".body").html(answer.body);
+    			}
+			});
+		}
+	</script>
+
+</body>
+</html>
